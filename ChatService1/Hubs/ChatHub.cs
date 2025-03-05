5f5d36e022ceccf5ff117c147794f7ed73b8c84c
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-05 16:44:47 +0200

    Configuring signalR

---
+++ ChatService1/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatService1.Hubs;
+
+public class ChatHub : Hub
+{
+    // Method to send a message to a specific user
+    public async Task SendMessage(string userId, string message)
+    {
+        await Clients.User(userId).SendAsync("ReceiveMessage", message);
+    }
+}
